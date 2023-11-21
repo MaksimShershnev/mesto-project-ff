6eedd5f8d3a80ad0bc26d846223338dffdcacd1e
MaksimShershnev <sir.mixmax@yandex.ru>
2023-11-21 16:05:36 +0300

    убраны обработчики кликов по кнопкам, фукнции обработки кликов. переделана функция создания карточек, рендер карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,72 +1,27 @@
-const cardTemplate = document.querySelector('#card-template').content;
-const content = document.querySelector('.content');
-const cardContainer = content.querySelector('.places__list');
-const editProfileButton = content.querySelector('.profile__edit-button');
-const addCardButton = content.querySelector('.profile__add-button');
-const closePopupButton = document.querySelectorAll('.popup__close');
+function createCard(card, handleDeleteCard) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-function cardRender() {
-  initialCards.forEach((card) => {
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    const deleteButton = cardElement.querySelector('.card__delete-button');
-    const likeButton = cardElement.querySelector('.card__like-button');
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__image').alt = card.name;
+  cardElement.querySelector('.card__title').textContent = card.name;
 
-    cardElement.querySelector('.card__image').src = card.link;
-    cardElement.querySelector('.card__image').alt = card.name;
-    cardElement.querySelector('.card__title').textContent = card.name;
-
-    deleteButton.addEventListener('click', deleteCard);
-    likeButton.addEventListener('click', likeCard);
-
-    cardContainer.append(cardElement);
-  });
-  showPopupCardImage();
+  deleteButton.addEventListener('click', handleDeleteCard);
+  return cardElement;
 }
 
-cardRender();
-
-function showPopupCardImage() {
-  const cardImage = content.querySelectorAll('.card__image');
-  const popupImage = document.querySelector('.popup_type_image');
-
-  cardImage.forEach((image) => {
-    image.addEventListener('click', (evt) => {
-      popupImage.querySelector('.popup__image').src = evt.target.src;
-      popupImage.querySelector('.popup__image').alt = evt.target.alt;
-      popupImage.querySelector('.popup__caption').textContent = evt.target.alt;
-      showPopup(popupImage);
-    });
-  });
-}
-
-function deleteCard(evt) {
+function handleDeleteCard(evt) {
   const cardItem = evt.target.closest('.card');
   cardItem.remove();
 }
 
-function likeCard(evt) {
-  evt.target.classList.toggle('card__like-button_is-active');
+function renderCard(card) {
+  const cardContainer = document.querySelector('.places__list');
+
+  cardContainer.prepend(createCard(card, handleDeleteCard));
 }
 
-function closePopup(evt) {
-  const popupItem = evt.target.closest('.popup');
-  popupItem.classList.remove('popup_is-opened');
-}
-
-function showPopup(popup) {
-  popup.classList.add('popup_is-opened');
-}
-
-editProfileButton.addEventListener('click', () => {
-  const popupProfileEdit = document.querySelector('.popup_type_edit');
-  showPopup(popupProfileEdit);
-});
-
-addCardButton.addEventListener('click', () => {
-  const popupNewCard = document.querySelector('.popup_type_new-card');
-  showPopup(popupNewCard);
-});
-
-closePopupButton.forEach((closeButton) => {
-  closeButton.addEventListener('click', closePopup);
+initialCards.forEach((cards) => {
+  renderCard(cards);
 });
